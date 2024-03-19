b474f1adef4ea0be9c2ab7f291e44dfeb322705e
cs2273520 <1329413417@qq.om>
2024-03-19 09:46:55 +0800

    why have the other content?

--- test.js
+++ test.js
@@ -1,27 +1,18 @@
 function test(){
     console.log("测试1提交1,但是我修改了内容");
     console.log("测试2第一次提交,我修改了2的内容113");
-<<<<<<< HEAD
 
-<<<<<<< HEAD
     console.log("提交第一次，现在进行amend修改测试12");
-=======
 
->>>>>>> 1adeb7d7174fc2e814e5e2fac13c3d462929fe24
-=======
     console.log("提交第一次，现在进行amend修改测试");
     console.log("dev，我是main分支");
 
 }
 function devide(){
     console.log("我是测试环境");
->>>>>>> devTest
 }
 function stash(){
-<<<<<<< HEAD
     console.log("我代码敲到一半需要保存，但是并不需要push");
-=======
     console.log("我代码敲到一半需要保存");
->>>>>>> 29ac897884354a0658b14020782072dcfe3d005e
 }
 
